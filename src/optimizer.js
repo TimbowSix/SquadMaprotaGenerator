74c67cca358154a95147c8f90155f92d92f51441
Kay Menkens-Siemers <kay_men@uni-bremen.de>
2022-08-30 23:03:40 +0200

    optimizer

--- src/optimizer.js
+++ src/optimizer.js
@@ -29,19 +29,46 @@ class Optimizer{
         //this.generator. TODO WO function
         this.update_dist();
         this.currentMin = this.calc_current_norm();
+
+        //TODO vielleicht über config
+        this.deltaStepSize = 0.01
     }
 
-    optimize_recursive(currentIndex, lowestDelta, minChanged){
+    optimize_recursive(currentIndex, lowestDelta, mapWeightKey, minChanged){
         if(this.delta <= lowestDelta){
             return
         }
         
-        this.generator.all_maps[currentIndex].map_weight += this.delta;
+        this.generator.all_maps[currentIndex].map_weight[mapWeightKey] += this.delta;
         //this.generator.generate //TODO 
         this.update_dist();
         cMin = this.calc_current_norm();
         if(this.currentMin > cMin){
-            
+            //new min found
+            this.currentMin = cMin;
+
+            console.log("new min: "+cMin);
+            console.group("mapweights");
+            for(let i=0;i<this.generator.all_maps.length;i++){
+                console.log(this.generator.all_maps[i].map_weight[mapWeightKey]);
+            }
+            console.groupEnd();
+
+            this.saveMapWeights();
+            this.optimize_recursive(currentIndex,lowestDelta,mapWeightKey, true)
+        }else{
+            //no new min found
+            this.generator.all_maps[currentIndex].map_weight[mapWeightKey] -= this.delta;
+            currentIndex++
+            if(currentIndex >= this.generator.all_maps.length){
+                currentIndex = 0
+                if(!minChanged){
+                    this.delta -= this.deltaStepSize
+                    console.log("new Delta "+this.delta)
+                    this.saveDelta();
+                }
+            }
+            this.optimize_recursive(currentIndex,lowestDelta,mapWeightKey,false)
         }
     }
 
@@ -68,7 +95,10 @@ class Optimizer{
         }
     }
     saveMapWeights(){
-        fs.writeFileSync("../data/mapweights.json", this.mapWeights)
+        fs.writeFileSync("../data/mapweights.json", this.mapWeights);
+    }
+    saveDelta(){
+        fs.writeFileSync("../data/delta.json", this.delta);
     }
 }
 
