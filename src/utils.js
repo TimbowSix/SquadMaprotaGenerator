cb7dac863b9a434339806c6b44a2268db2b1a342
Timbow <timbowsix@gmail.com>
2022-08-30 19:19:26 +0200

    formatLayer

--- src/utils.js
+++ src/utils.js
@@ -39,6 +39,33 @@ function multiplyArr(arr1, arr2){
       })
     return n_arr
 }
+function formatLayer(layer){
+    /**
+     * formats a layer string to preffered readable casing format.\n
+     * If mode or map is not found it only gets to lower case\n
+     * layer string is expected to have following standard format: map_mode_version
+    **/
+    layer = layer.toLowerCase()
+    let values = layer.split("_")
+    let map =  values[0]
+    let mode = values[1]
+    let ver = values[2]
+    let modes = {"aas": "AAS", "raas": "RAAS", "tc": "TC", "ta": "TA", "invasion": "Invasion", "destruction": "Destruction","insurgency": "Insurgency", "seed": "Seed"}
+    mode = modes.hasOwnProperty(mode) ? modes[mode] : mode
+
+    maps = {'albasrah': 'AlBasrah','anvil': 'Anvil','belaya': 'Belaya','blackcoast': 'BlackCoast','chora': 'Chora','fallujah': 'Fallujah',
+    'foolsroad': 'FoolsRoad','goosebay': 'GooseBay','gorodok': 'Gorodok','kamdesh': 'Kamdesh','kohat': 'Kohat','kokan': 'Kokan','lashkarvalley': 'LashkarValley',
+    'logar': 'Logar','manic': 'Manic','mestia': 'Mestia','mutaha': 'Mutaha','narva': 'Narva','skorpo': 'Skorpo','sumari': 'Sumari','tallil': 'Tallil',
+    'yehorivka': 'Yehorivka'
+    }
+
+    map = maps.hasOwnProperty(map) ? maps[map] : map
+
+    return `${map}_${mode}_${ver}`
+}
+
+
+
 
 
 function main(){
@@ -46,7 +73,8 @@ function main(){
 }
 
 if (require.main === module) {
-    main()
+    let test =  "koKan_rAAs_v1"
+    console.log(formatLayer(test))
 }
 
-module.exports = { weightedChoice, normalize, squareArr, sumArr, multiplyArr };
+module.exports = { weightedChoice, normalize, squareArr, sumArr, multiplyArr, formatLayer };
