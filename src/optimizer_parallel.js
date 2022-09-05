8a4e655f0693122596d38864d5b0c32883c29216
Kay Menkens-Siemers <kay_men@uni-bremen.de>
2022-09-05 19:16:07 +0200

    multithreading wiederherstellen, noch nicht lauffähig

--- src/optimizer_parallel.js
+++ src/optimizer_parallel.js
@@ -1,5 +1,9 @@
-const {Worker, workerData} = require('node:worker_threads')
+const {Worker} = require('node:worker_threads')
 
-const main_pool_worker = new Worker("./src/optimizer_worker.js",{ workerData: 'main' });
-const intermediate_pool_worker = new Worker("./src/optimizer_worker.js",{ workerData: 'intermediate' });
-const rest_pool_worker = new Worker("./src/optimizer_worker.js",{ workerData: 'rest' });
+const raas_pool_worker = new Worker("./src/optimizer_worker.js",{ workerData: {"mode": 'RAAS', "dist": null} });
+//const aas_pool_worker = new Worker("./src/optimizer_worker.js",{ workerData: {"mode": 'AAS', "dist": null} });
+//const inv_pool_worker = new Worker("./src/optimizer_worker.js",{ workerData: {"mode": 'Invasion', "dist": null} });
+//const tc_pool_worker = new Worker("./src/optimizer_worker.js",{ workerData: {"mode": 'TC', "dist": null} });
+//const des_pool_worker = new Worker("./src/optimizer_worker.js",{ workerData: {"mode": 'Destruction', "dist": null} });
+//const ins_pool_worker = new Worker("./src/optimizer_worker.js",{ workerData: {"mode": 'Insurgency', "dist": null} });
+
