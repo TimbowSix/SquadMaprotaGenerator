d0cf101151cdc023edf1ea9badae696f6a031fe2
Kay Menkens-Siemers <kay_men@uni-bremen.de>
2022-09-06 19:15:36 +0200

    neuerungen opimizer parallel

--- src/optimizer_parallel.js
+++ src/optimizer_parallel.js
@@ -1,9 +1,12 @@
-const {Worker} = require('node:worker_threads')
+const {Worker, parentPort} = require('node:worker_threads')
 
-const raas_pool_worker = new Worker("./src/optimizer_worker.js",{ workerData: {"mode": 'RAAS', "dist": null} });
+//const raas_pool_worker = new Worker("./src/optimizer_worker.js",{ workerData: {"mode": 'RAAS', "dist": null} });
 //const aas_pool_worker = new Worker("./src/optimizer_worker.js",{ workerData: {"mode": 'AAS', "dist": null} });
 //const inv_pool_worker = new Worker("./src/optimizer_worker.js",{ workerData: {"mode": 'Invasion', "dist": null} });
 //const tc_pool_worker = new Worker("./src/optimizer_worker.js",{ workerData: {"mode": 'TC', "dist": null} });
 //const des_pool_worker = new Worker("./src/optimizer_worker.js",{ workerData: {"mode": 'Destruction', "dist": null} });
-//const ins_pool_worker = new Worker("./src/optimizer_worker.js",{ workerData: {"mode": 'Insurgency', "dist": null} });
-
+const ins_pool_worker = new Worker("./src/optimizer_worker.js",{ workerData: {"mode": 'Insurgency', "dist": null} });
+        
+ins_pool_worker.once('return', (msg) => {
+    console.log(msg)
+})
--- src/optimizer_worker.js
+++ src/optimizer_worker.js
@@ -1,14 +1,15 @@
-const {workerData} = require('node:worker_threads')
+const {workerData, parentPort} = require('node:worker_threads')
 const opt = require('./optimizer.js')
 const config = require("../config.json")
 const { fstat } = require('node:fs')
-const fs = require("fs")
+const fs = require("fs") 
 
-console.log("test")
-console.log(workerData)
 console.log("start optimizer for "+ workerData["mode"])
-//op = new opt.Optimizer(config, workerData["mode"], reset=true, distribution = workerData["dist"], console_output = false, use_extern_map_weights_and_delta = false,save_maps=true,start_delta = 0.5, estimate = false)
+op = new opt.Optimizer(config, workerData["mode"], reset=true, distribution = workerData["dist"], console_output = false, use_extern_map_weights_and_delta = false,save_maps=true,start_delta = 0.5, estimate = false)
 console.time("Execution Time")
-//result = op.start_optimizer()
-//fs.writeFile("result_"+workerData["mode"]+"_"+Date.now()+".json",JSON.stringify(result,null,2))
+result = op.start_optimizer()
+parentPort.postMessage(result)
 console.timeEnd("Execution Time")
+return result
+fs.writeFileSync("result_"+workerData["mode"]+"_"+Date.now()+".json",JSON.stringify(result,null,2))
+
