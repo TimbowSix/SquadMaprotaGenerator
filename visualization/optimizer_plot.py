c659a7e1a1d40a88321ccd422c089033f8d3e595
Timbow <timbowsix@gmail.com>
2022-09-12 23:45:46 +0200

    optimizer_plot + run_info_plot combine

--- visualization/optimizer_plot.py
+++ visualization/optimizer_plot.py
@@ -1,61 +1,117 @@
 import plotly.express as px
 import pandas as pd
-import os, json, shutil
+import json
+from PIL import Image
+import tempfile
+import math
 import re
 import imageio.v2 as imageio
+import os
 
-## Setup
 
-history_path = "optimizer_maps_history_AAS_1662668217656.json"
-mode = "AAS"
+#-# Config #-#
+
+run_info_path = "run_info_eb191fb1-b617-4a50-a315-d5ad83111b79_AAS.json"
+history_path = "optimizer_maps_history_AAS_eb191fb1-b617-4a50-a315-d5ad83111b79.json"
+deviation = True
+plot_template = "plotly_dark"
+save_single_pics=False
 gif_duration = 0.5
-custom_title = None
 
+mode = run_info_path.split("_")[-1].replace(".json", "")
+#-#
 
 def sorted_alphanumeric(data):
     convert = lambda text: int(text) if text.isdigit() else text.lower()
     alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
     return sorted(data, key=alphanum_key)
 
-def main():
-    path = "/.temp"
-    exist = os.path.exists(path)
-    if not exist:
-        os.makedirs(path)
-        
-    with open(history_path, "r") as f:
-        file = json.load(f)
+def num_zeros(decimal):
+    return -math.floor(math.log10(decimal)) - 1
 
-    dfs = []
-    max_ = 0
-    for ind, maps in enumerate(file):
-        count = [{"map": str(map_), "count": count} for map_ , count in maps[mode].items()]
-        df = pd.DataFrame(count)
-        max_n = round(df["count"].max()+500, -3)
-        max_ = max_n if max_n > max_ else max_
-        df.sort_values(by="count", inplace=True, ascending=False)
-        dfs.append(df)
+def round_x_decimal(num):
+    return round(num+5*10**-(num_zeros(num)+2), num_zeros(num)+1)  
 
+with open(run_info_path, "r") as fr, open(history_path, "r") as fh:
+    run_info = json.load(fr)
+    history = json.load(fh)
+
+# Gif
+dfs = []
+max_ = 0
+for ind, maps in enumerate(history):
+    count = [{"map": str(map_), "count": count} for map_ , count in maps[mode].items()]
+    df = pd.DataFrame(count)
+    max_n = round(df["count"].max()+500, -3)
+    max_ = max_n if max_n > max_ else max_
+    df.sort_values(by="count", inplace=True, ascending=False)
+    dfs.append(df)
+
+with tempfile.TemporaryDirectory() as t_dir:
     for ind, df in enumerate(dfs):
-        title = f"{ind}" if custom_title is None else custom_title
+        title = f"{ind} {mode}"
         fig = px.bar(df, x="map", y="count", template="plotly_dark", title=title)
         fig.update_xaxes(categoryorder="category ascending")
         fig.update_yaxes(range=[0, max_])
-        fig.write_image(f"{path}/fig_{ind}.png")
+        fig.write_image(f"{t_dir}/fig_{ind}.png")
         if ind == 0:
             fig.write_image(f"first.png")
         elif ind == len(dfs)-1:
             fig.write_image(f"last.png")
 
-    images = os.listdir(path)
-    images = sorted_alphanumeric(os.listdir(path))
+    images = sorted_alphanumeric(os.listdir(t_dir))
     images_read = []
     for filename in images:
         if ".png" in filename:
-            images_read.append(imageio.imread(f"{path}/{filename}"))
+            images_read.append(imageio.imread(f"{t_dir}/{filename}"))
     imageio.mimsave(f'output.gif', images_read, duration=gif_duration)
 
-    shutil.rmtree("/.temp")
+# history / run_info comparison
 
-if __name__ == "__main__":
-    main()
+ri = [{"map": str(map_), "count": count} for map_ , count in run_info.items()]
+run_info_df = pd.DataFrame(ri)
+run_info_df.sort_values(by="map", inplace=True, ascending=True)
+run_info_df.reset_index(drop=True,inplace=True)
+
+h = [{"map": str(map_), "count": count} for map_ , count in history[-1][mode].items()]
+history_df = pd.DataFrame(h)
+history_df.sort_values(by="map", inplace=True, ascending=True)
+history_df.reset_index(drop=True, inplace=True)
+
+history_df["percentage"] = history_df["count"]/history_df["count"].sum()  
+
+max_h = round_x_decimal(history_df["percentage"].max())
+max_r = round_x_decimal(run_info_df["count"].max())
+
+max_value = max((max_h, max_r))
+
+title = f"History {mode}"
+history_plot = px.bar(history_df, x="map", y="percentage", template=plot_template, title=title)
+history_plot.update_xaxes(categoryorder="category ascending")
+history_plot.update_yaxes(range=[0, max_value])
+title = f"Run Info {mode}"
+run_info_plot = px.bar(run_info_df, x="map", y="count", template=plot_template, title=title)
+run_info_plot.update_xaxes(categoryorder="category ascending")
+history_plot.update_yaxes(range=[0, max_value])
+
+if deviation:
+    history_df["deviation"] = history_df["percentage"]-run_info_df["count"]
+    title = f"History-Run Info Deviation {mode}"
+    deviation_plot = px.bar(history_df, x="map", y="deviation", template=plot_template, title=title)
+    deviation_plot.update_xaxes(categoryorder="category ascending")
+    deviation_plot.write_image("deviation.png")
+
+with tempfile.TemporaryFile() as hist, tempfile.TemporaryFile() as run:
+    history_plot.write_image(hist)
+    history_img = Image.open(hist)
+    run_info_plot.write_image(run)
+    run_info_img = Image.open(run)
+
+    if save_single_pics:
+        history_plot.write_image("history.png")
+        run_info_plot.write_image("run_info.png")
+
+    combined = Image.new('RGB', (history_img.width + run_info_img.width, history_img.height))
+    combined.paste(history_img, (0, 0))
+    combined.paste(run_info_img, (history_img.width, 0))
+    combined.save("history_run_info.png")
