6bceeda6d011ae54adaf8ca93f78a223ffcc4405
Timbow <timbowsix@gmail.com>
2022-09-18 20:29:47 +0200

    change check

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
 const generator = require("./src/generator.js")
 const fs = require("fs")
+const optimizer = require("./src/optimizer_parallel.js")
+const crypto = require("crypto")
 
 function main(){
     let config = require("./config.json")
+    if (config["auto_optimize"]){
+        let save = require("./data/save.json")
+        let check = {}
+        check["bioms"] = crypto.createHash("md5").update(JSON.stringify(require("./data/bioms.json"))).digest("hex")
+        let c_config = {}
+        c_config["biom_spacing"] = config["biom_spacing"]
+        c_config["mode_distribution"] = config["mode_distribution"] 
+        c_config["use_lock_time_modifier"] = config["use_lock_time_modifier"]
+        check["config"] = crypto.createHash("md5").update(JSON.stringify(c_config)).digest("hex")
+
+        let old_hash = crypto.createHash("md5").update(JSON.stringify(save)).digest("hex")
+        let new_hash = crypto.createHash("md5").update(JSON.stringify(check)).digest("hex")
+        if(old_hash != new_hash){
+            console.log(`WARNING: relevant data values changed, running optimizer`)
+            fs.writeFileSync("./data/save.json", JSON.stringify(check))
+            optimizer.start_optimizer_parallel(main)
+            return
+        }
+    }
+
     for(let i=0; i<config["number_of_rotas"]; i++){
         let gen = new generator.Maprota(config)
         let rota = gen.generate_rota()
