f824f7b19b3eb670efe470893b855bbc0e0a3a86
Timbow <timbowsix@gmail.com>
2022-09-22 22:27:30 +0200

    change check to data

--- main.js
+++ main.js
@@ -2,25 +2,15 @@ const generator = require("./src/generator.js")
 const fs = require("fs")
 const optimizer = require("./src/optimizer_parallel.js")
 const crypto = require("crypto")
+const data = require("./src/data.js")
+
 
 function main(){
     let config = require("./config.json")
-    if (config["auto_optimize"]){
-        let save = JSON.parse(fs.readFileSync("./data/save.json"))
-        let check = {}
-        check["bioms"] = crypto.createHash("md5").update(JSON.stringify(require("./data/bioms.json"))).digest("hex")
-        let c_config = {}
-        c_config["biom_spacing"] = config["biom_spacing"]
-        c_config["mode_distribution"] = config["mode_distribution"] 
-        c_config["use_lock_time_modifier"] = config["use_lock_time_modifier"]
-        check["config"] = crypto.createHash("md5").update(JSON.stringify(c_config)).digest("hex")
-
-        if(crypto.createHash("md5").update(JSON.stringify(save)).digest("hex") != crypto.createHash("md5").update(JSON.stringify(check)).digest("hex")){
-            console.log(`INFO: relevant data values changed, running optimizer`)
-            fs.writeFileSync("./data/save.json", JSON.stringify(check))
-            optimizer.start_optimizer_parallel(main)
-            return
-        }
+    if (config["auto_optimize"] && data.check_changes()){
+        console.log(`INFO: relevant data values changed, running optimizer`)
+        optimizer.start_optimizer_parallel(main)
+        return
     }
     console.log("Starting Rota generation")
     console.time("Generation Time")
