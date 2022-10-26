b0042cfc5bc33f353386b6c3e2cd7ef27292b25a
Kay Menkens-Siemers <kay_men@uni-bremen.de>
2022-10-26 17:45:20 +0200

    utils get_maps_modi_dict

--- src/utils.ts
+++ src/utils.ts
@@ -33,6 +33,49 @@ import fs from fs
  }
 
 
+
+ function get_maps_modi_dict(maps: Array<RotaMap>, modi: Array<Mode>){
+    let dict: Map<Mode, Map<RotaMap, number>> = new Map()
+    for(let map of maps){
+        for(let mode of modi){
+            if(dict.get(mode) == undefined){
+                dict.set(mode, new Map())
+            }
+
+            if(map.map_weight.get(mode)){
+                dict.get(mode)?.set(map, Math.random())
+            }
+            else{
+                dict.get(mode)?.set(map, 0)
+            }
+
+        }
+    }
+
+    for(let mode of dict.keys()){
+        let sum = 0
+        let temp = dict.get(mode)
+
+        if(temp == undefined) throw Error("no mode in maps modi dict")
+
+        for(let weight of temp.values()){
+            sum+=weight
+        }
+        for(let weight_key of temp.keys()){
+            let curr: number | undefined = dict.get(mode)?.get(weight_key)
+
+            if(curr == undefined) throw Error ("no number in maps modi dict")
+
+            dict.get(mode)?.set(weight_key, (curr/sum))
+        }
+
+    }
+
+    return dict
+}
+
+
+
  /**
  * rounds number to x decimal places
  * @param {number} val number
@@ -50,4 +93,15 @@ function round(val: number, digits:number =2){
  */
  function sumArr(arr: Array<number>){
     return arr.reduce(function(pv, cv) { return pv + cv; }, 0)
+}
+
+
+function create_UUID(): string{
+    let dt = new Date().getTime();
+    let uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        let r = (dt + Math.random()*16)%16 | 0;
+        dt = Math.floor(dt/16);
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16);
+    });
+    return uuid;
 }
