b87341b3cc1b7f0fad0a706c3a7b9ffac00b0d43
Kay <kayms@live.de>
2023-02-11 17:42:27 +0100

    version up

--- src/generator/GlobalConfig.hpp
+++ src/generator/GlobalConfig.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
 #include <random>
-#define ROTA_VERSION_MAJOR 2
-#define ROTA_VERSION_MINOR 2
+#define ROTA_VERSION_MAJOR 3
+#define ROTA_VERSION_MINOR 0
 #define CONFIG_PATH "/etc/maprota/"
 
 #define WEIGHT_PARAMS_COUNT 6
